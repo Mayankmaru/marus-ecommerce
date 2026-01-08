f8d840a58120951fa88aba3db34894b65f659535
Your Name <you@example.com>
2026-01-08 16:16:49 +0530

    Fix product image migration, add defaults, stabilize order models

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
 
 from django.contrib import admin
-from .models import Product
+from .models import OrderItem, Product, Order, OrderItem
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
